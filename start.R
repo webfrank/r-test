0343a3a4c2e01e4e58561c6237e8310314cc7541
Francesco Boscarino <francesco@bizmate.it>
2017-04-30 20:15:04 +0200

    dnorm

---
+++ start.R
@@ -0,0 +1,9 @@
+library("rjson")
+
+# Create a sequence of numbers between -10 and 10 incrementing by 0.1.
+x <- seq(-10, 10, by = .1)
+
+# Choose the mean as 2.5 and standard deviation as 0.5.
+y <- dnorm(x, mean = 2.5, sd = 0.5)
+
+toJSON(y)
